5de0a20801c83da18780ee4b98b755fdb4eff20e
jhnlrnzo <luisjohnlrnzo@gmail.com>
2024-08-16 00:13:31 +0800

    Update task3.java
    tasksheet-1-1-3

--- task3.java
+++ task3.java
@@ -1,1 +1,26 @@
+public class task3 {
+    public static void main(String[] args) {
+
+        // This is for number "3".
+        int e = 3;
+        
+        // This is for double "1".
+        byte l = 1;
+        
+        // Also for number "o".
+        byte o = 0;
+
+        int five = 114;
+        char r = (char)five;
+
+        int six = 100;
+        char d = (char )six;
+        
+
+        String output = ("H"+ e + l + l + o + " w" + o + r + l + d + " " + 2.0f + " " + true);
+        System.out.println(output);
+
+    }
+} 
+
 

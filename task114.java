18905db8ef9eee78d8486c1c1f374b195cb304e8
jhnlrnzo <luisjohnlrnzo@gmail.com>
2024-08-16 20:27:43 +0800

    Create task114.java

---
+++ task114.java
@@ -0,0 +1,15 @@
+
+public class task114 {
+    
+    public static void main(String[] args) {
+        for (int i=0; i <= 10; ++i){
+            int a = i;
+            if (a % 2 == 0){
+                System.out.println(i + " is even.");
+            } else {
+                System.out.println(i + " is odd.");
+            }
+        }
+    }
+    
+}

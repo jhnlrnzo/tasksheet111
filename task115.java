9e9f10627abc5b4ae1bdae0a93e8983047b85aa5
jhnlrnzo <luisjohnlrnzo@gmail.com>
2024-08-20 14:17:23 +0000

    Success!!!

--- task115.java
+++ task115.java
@@ -1,1 +1,16 @@
+public class task115{
+    public static void main(String[] args) {
+        String a = "Wow";
+        String b = "Wow";
+        String d = (b + "!");
+        String c = d;
 
+        boolean b1 = (a == b); // a must equal to b
+        boolean b2 = d.equals(b + "!"); // d must not equal to b
+        boolean b3 = !c.equals(a); // c must not equal to a
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
+}

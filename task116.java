677d758011dde415183184c2b40c6bc1a2af572a
jhnlrnzo <luisjohnlrnzo@gmail.com>
2024-08-20 14:56:37 +0000

    panlidrome

--- task116.java
+++ task116.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class task116 {
+    public static void main(String[] args) {
+        Scanner panlidrome = new Scanner(System.in);
+        System.out.println("Enter any word: ");
+
+        String isit = panlidrome.nextLine();
+        String itis = "";
+        for (int i = 0; i < isit.length(); i++){
+            itis = isit.charAt(i) + itis;
+        }
+        if (isit.equals(itis)){
+            System.out.println("The input is panlidrome!");
+        } else {
+            System.out.println("The input is not panlidrome!");
+        }
+    }
+}

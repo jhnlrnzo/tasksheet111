4bc42e63ef4c91b90fd454cb333cd4f362f6fc7b
jhnlrnzo <luisjohnlrnzo@gmail.com>
2024-08-29 14:55:14 +0000

    Cumulative Sum

--- task120.java
+++ task120.java
@@ -1,1 +1,16 @@
+public class task120   {
+    public static void total(int... nums){
+        int sum = 0;
+        for (int num: nums){
+            for (int i = 0; i <= num; i++){
+                sum = sum + i;
+                System.out.println(sum);
+            }
+            sum = 0;
+        }      
+    }
+    public static void main(String[] args) {
+        total(4,5,10);
+    }
+}
 

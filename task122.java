9caf180ed1896ff85b14d66e86f738cff002f323
jhnlrnzo <luisjohnlrnzo@gmail.com>
2024-09-03 02:35:32 +0000

    encaptulation

--- task122.java
+++ task122.java
@@ -1,1 +1,25 @@
+class Student{
+    private String fName;
+    private String lName;
+    public Student(String fName, String lName){
+        this.fName = fName;
+        this.lName = lName;
+    }
+    void printFullName(String fullName){
+        System.out.println(fullName = fName + " " + lName);
+    }
+}
 
+
+public class task122 {
+    public static void main(String[] args) {
+        Student[] students = new Student[] {
+            new Student("Morgan", "Freeman"),
+            new Student("Brad", "Pitt"),
+            new Student("Kevin", "Spacey"),
+        };
+        for (Student s : students) {
+            s.printFullName(null);
+        }
+    }
+}
